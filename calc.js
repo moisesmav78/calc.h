4148f94dd6532357ad1a3b4817550e5625592e50
Moises <moisesmavo500@gmail.com>
2023-08-28 12:41:21 -0400

    calculadora completa sin case

--- calc.js
+++ calc.js
@@ -1,36 +1,35 @@
-// comentario de una linea
-/* comentario de más
-de una línea*/
-const prompt = require("prompt-sync")(); //esto para pdoer usar prompt
-// ejecutar por terminal: npm install prompt-sync
-// indentar control +k control +F
-
-var nombre;
-var n1, n2, suma;
-
-seguir = prompt("Bienvenido a la calculadora, ¿quiere seguir? s/n: ");
+seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
 if (seguir == "s") {
-    opcion = parseInt(prompt("1.- Suma, 2.- Resta, 3.- Multiplicación, 4.- División, ¿quiere hacer?:"));
-    if (opcion ==1){
-        console.log ("esto es suma")
+    opcion = prompt("1.- suma, 2.- resta 3.- Multiplicacion 4.- Division, que quiere hacer? ");
+    if (opcion == 1) {
+        console.log("esto es suma ")
         n1 = parseInt(prompt("Ingrese número 1: "));
         n2 = parseFloat(prompt("Ingrese número 2: "));
-        suma = n1 + n2;
-        console.log(suma)
+        resultado = n1 + n2;
+        console.log(resultado);
     }
-    else if (opcion ==2)
-    {
-        console.log ("esto es resta")
+    else if (opcion == 2) {
+    console.log(" Esto es resta")
+    n1 = parseInt(prompt("Ingrese número 1: "));
+    n2 = parseFloat(prompt("Ingrese número 2: "));
+    resultado = n1 - n2;
+    console.log(resultado)
     }
-    else if (opcion ==3){
-        console.log ("esto es multiplicacion")
+    else if (opcion == 3) {
+    console.log(" Esto es Multiplicación ")
+    n1 = parseInt(prompt("Ingrese número 1: "));
+    n2 = parseFloat(prompt("Ingrese número 2: "));
+    resultado = n1 * n2;
+    console.log(resultado)
     }
-    else {
-        console.log ("esto es division")
-    }
+    else 
+    console.log(" Esto es division ")
+    n1 = parseInt(prompt("Ingrese número 1: "));
+    n2 = parseFloat(prompt("Ingrese número 2: "));
+    resultado = n1 / n2;
+    console.log(resultado)
 }
-else
-    console.log("Chau");
+
 
 
 
