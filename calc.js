4d736405ceab9abdd0cd521e4f08b6b82515017c
Moises Mavo <moisesmavo500@gmail.com>
2023-08-31 11:39:07 -0400

    Actualizado

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
-seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
+const prompt = require("prompt-sync")(); 
+/*seguir = prompt("Bienvenido a la calculadora, quiere seguir? s/n: ");
 if (seguir == "s") {
     opcion = prompt("1.- suma, 2.- resta 3.- Multiplicacion 4.- Division, que quiere hacer? ");
     if (opcion == 1) {
@@ -29,7 +30,44 @@ if (seguir == "s") {
     resultado = n1 / n2;
     console.log(resultado)
 }
+*/
 
-
+seguir = prompt("Bienvenido a la calculadora, ¿quieres seguir? s/n: ");
+if (seguir == "s") {
+  opcion = prompt("1.- suma, 2.- resta 3.- Multiplicacion 4.- Division, ¿qué quieres hacer? ");
+  switch (opcion) {
+    case "1":
+      console.log("Esto es suma");
+      n1 = parseInt(prompt("Ingrese número 1: "));
+      n2 = parseFloat(prompt("Ingrese número 2: "));
+      resultado = n1 + n2;
+      console.log(resultado);
+      break;
+    case "2":
+      console.log("Esto es resta");
+      n1 = parseInt(prompt("Ingrese número 1: "));
+      n2 = parseFloat(prompt("Ingrese número 2: "));
+      resultado = n1 - n2;
+      console.log(resultado);
+      break;
+    case "3":
+      console.log("Esto es multiplicación");
+      n1 = parseInt(prompt("Ingrese número 1: "));
+      n2 = parseFloat(prompt("Ingrese número 2: "));
+      resultado = n1 * n2;
+      console.log(resultado);
+      break;
+    case "4":
+      console.log("Esto es división");
+      n1 = parseInt(prompt("Ingrese número 1: "));
+      n2 = parseFloat(prompt("Ingrese número 2: "));
+      resultado = n1 / n2;
+      console.log(resultado);
+      break;
+    default:
+      console.log("Opción inválida");
+      break;
+  }
+}
 
 
